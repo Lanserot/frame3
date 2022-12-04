b1237c77c2171ad9adacb417a790fe519b3b6773
lanserot <v-256@mail.ru>
2022-12-04 18:50:15 +0300

    tests

---
+++ tests/tools/StrClassTest.php
@@ -0,0 +1,29 @@
+<?php
+
+use Core\Tools\StrClass;
+use PHPUnit\Framework\TestCase;
+
+class StrClassTest extends TestCase
+{
+    /**
+     * @dataProvider addressProvider
+     * @return void
+     */
+    public function testBasic($generate)
+    {
+        $text = StrClass::handleRandomStringInArray($generate);
+        $this->assertNotEmpty($text);
+    }
+
+    public function addressProvider(): array
+    {
+        return [
+            [
+                ['|randString|']
+            ],
+            [
+                ['|randString|@mail.ru']
+            ]
+        ];
+    }
+}

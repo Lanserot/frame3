5da4383de6ee809676ed5b96dcf3e9731443a791
lanserot <v-256@mail.ru>
2023-01-26 23:00:33 +0300

    up readme, add file

---
+++ public/User/login.php
@@ -0,0 +1,12 @@
+<?php
+
+use Core\Route\Route;
+
+?>
+<div class="container">
+    <form action="<?= Route::route('login') ?>" method="POST">
+        <p>Login <input type="text" name='login'></p>
+        <p>Pass <input type="text" name='pass'></p>
+        <button type="submit">login</button>
+    </form>
+</div>
